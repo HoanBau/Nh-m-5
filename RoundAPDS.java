89d8f651f67f22c5a501eb65c8beded40a0998b2
HoanBau <148040039+HoanBau@users.noreply.github.com>
2023-10-16 13:25:51 +0700

    Add files via upload

---
+++ RoundAPDS.java
@@ -0,0 +1,10 @@
+package TankGun;
+
+public class RoundAPDS extends Round {
+        
+    public RoundAPDS(int calibre){
+        super(calibre, "APDS");
+        System.out.println("    Round type: APDS");
+    }
+}
+
